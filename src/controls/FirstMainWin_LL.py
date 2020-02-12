49e10f8a273d7c52107f6789f1c17ef62abcb147
875348270@qq.com <875348270@qq.com>
2020-02-12 16:26:09 +0800

    增加自己的一个测试文件

---
+++ src/controls/FirstMainWin_LL.py
@@ -0,0 +1,17 @@
+import sys
+from PySide2.QtWidgets import QApplication,QMainWindow
+from PySide2.QtGui import QIcon
+class FistMainWin(QMainWindow):
+    def __init__(self):
+        super(FistMainWin,self).__init__()
+        self.setWindowTitle("第一个应用程序")
+        self.setWindowIcon(QIcon("./images/Dragon.ico"))
+        self.resize(400,300)
+        self.status=self.statusBar()
+        self.status.showMessage("只存在5秒的消息",5000)
+if __name__ == "__main__" :
+    app = QApplication(sys.argv)
+    win = FistMainWin()
+    win.show()
+    sys.exit(app.exec_())
+    
